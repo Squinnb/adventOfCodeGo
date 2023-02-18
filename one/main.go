e5873c705df79d3300fe5012bcf60c26fe79524f
Quinn <squinnmb@gmail.com>
2023-02-18 17:17:42 -0600

    Day one complete with go.

--- one/main.go
+++ one/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -13,10 +14,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	m := 0
+
 	text := string(txtF) // Convert []byte to string
 	elves := strings.Split(text, "\n\n")
-	for _, e := range elves {
+	one(elves)
+	two(elves)
+}
+func one(elfslc []string) {
+	m := 0
+	for _, e := range elfslc {
 		elfCals := 0
 		for _, cal := range strings.Split(e, "\n") {
 			elfCal, err := strconv.Atoi(cal)
@@ -29,5 +35,30 @@ func main() {
 			m = elfCals
 		}
 	}
-	fmt.Printf("Big elf: %d", m)
+	fmt.Printf("Big elf: %d \n", m)
+}
+func two(elfslc []string) {
+	m := []int{0, 0, 0}
+	for _, e := range elfslc {
+		elfCals := 0
+		for _, cal := range strings.Split(e, "\n") {
+			elfCal, err := strconv.Atoi(cal)
+			if err != nil {
+				fmt.Print(err)
+			}
+			elfCals += elfCal
+		}
+		if elfCals > m[2] {
+			m[2] = elfCals
+			sort.Ints(m)
+		} else if elfCals > m[1] {
+			m[1] = elfCals
+			sort.Ints(m)
+		} else if elfCals > m[0] {
+			m[0] = elfCals
+			sort.Ints(m)
+		}
+	}
+	sum := m[0] + m[1] + m[2]
+	fmt.Printf("Big elf two: %d", sum)
 }
